c4a8db48aed6de59a103fe16e5ab860348b0f7c8
mhkoji <mhkoji2+git@gmail.com>
2018-06-25 23:12:16 +0900

    Refactor

--- cocoa/src/use-case/image/add.lisp
+++ cocoa/src/use-case/image/add.lisp
@@ -7,15 +7,15 @@
   (list :id (cocoa.entity.fs.image:image-id image)
         :path (cocoa.entity.fs.image:image-path image)))
 
-(defstruct add-images image-factory image-dao)
-(export 'make-add-images)
+@export
+(defun make-add-images (&key image-factory image-dao)
+  (lambda (paths)
+    (let ((images (cocoa.entity.fs.image:make-by-paths image-factory
+                                                       paths)))
+      (cocoa.entity.fs.image:save image-dao images)
+      (mapcar #'image->dto images))))
 
 @export
 (defun call (add-images paths)
   "The use case of adding images"
-  (let ((images (cocoa.entity.fs.image:make-by-paths
-                 (add-images-image-factory add-images)
-                 paths)))
-    (cocoa.entity.fs.image:save (add-images-image-dao add-images)
-                                images)
-    (mapcar #'image->dto images)))
+  (funcall add-images paths))
