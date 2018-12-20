36361bb1f257a9c98fa2993c5fcd25b7caaf19f9
mhkoji <mhkoji2+git@gmail.com>
2018-12-20 21:53:15 +0900

    Gtk: add table

--- vase-app/src/gtk/main.lisp
+++ vase-app/src/gtk/main.lisp
@@ -24,28 +24,45 @@
                         (lambda (widget)
                           (declare (ignore widget))
                           (leave-gtk-main)))
-      (let ((scrolled (make-instance 'gtk-scrolled-window
-                                     :border-width 12
-                                     :hscrollbar-policy :automatic
-                                     :vscrollbar-policy :always)))
 
-        (let ((vgrid (make-instance 'gtk-grid
-                                    :orientation :vertical
-                                    :border-width 8)))
-          (with-container (c conf)
-            (with-accessors ((db container-db)
-                             (image-repos container-image-repository)
-                             (folder-repos container-folder-repository)) c
-              (dolist (f (vase.folder:bulk-load-by-range folder-repos
-                                                         0
-                                                         50))
-                (let* ((pixbuf (-> (gdk-pixbuf-new-from-file
-                                    (vase.image:image-path
-                                     (vase.folder:folder-thumbnail f)))
-                                   (gdk-pixbuf-scale-simple 480 640 :hyper)))
-                       (image (gtk-image-new-from-pixbuf pixbuf)))
-                  (gtk-container-add vgrid image)))))
-          (gtk-container-add scrolled vgrid))
-        (gtk-container-add window scrolled))
+      (let ((table (make-instance 'gtk-table
+                                  :n-columns 1
+                                  :n-rows 20
+                                  :homogeneous t)))
+        (let ((scrolled (make-instance 'gtk-scrolled-window
+                                       :border-width 12
+                                       :hscrollbar-policy :automatic
+                                       :vscrollbar-policy :always)))
+          (let ((vgrid (make-instance 'gtk-grid
+                                      :orientation :vertical
+                                      :border-width 8)))
+            (with-container (c conf)
+              (with-accessors ((db container-db)
+                               (image-repos container-image-repository)
+                               (folder-repos container-folder-repository)) c
+                (dolist (f (vase.folder:bulk-load-by-range folder-repos
+                                                           0
+                                                           50))
+                  (let* ((pixbuf
+                          (-> (gdk-pixbuf-new-from-file
+                               (vase.image:image-path
+                                (vase.folder:folder-thumbnail f)))
+                              (gdk-pixbuf-scale-simple 300 300 :hyper)))
+                         (image (gtk-image-new-from-pixbuf pixbuf)))
+                    (gtk-container-add vgrid image)))))
+            (gtk-container-add scrolled vgrid))
+          (gtk-table-attach table scrolled 0 1 0 18))
+
+      (let ((prev-button (gtk-button-new-with-label "<"))
+            (next-button (gtk-button-new-with-label ">"))
+            (button-box (make-instance 'gtk-box
+                                       :orientation :horizontal
+                                       :spacing 12
+                                       :border-width 12)))
+        (gtk-box-pack-start button-box prev-button)
+        (gtk-box-pack-start button-box next-button)
+        (gtk-table-attach table button-box 0 1 18 20))
+
+      (gtk-container-add window table))
 
       (gtk-widget-show-all window))))
